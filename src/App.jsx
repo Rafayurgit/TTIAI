cd1764d39f8089bb6d3089a8a4017e98a0a17ec4
farruop <rafayKhanpvt@gmail.com>
2025-01-17 23:24:36 +0530

    Add: Router to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react';
+import {Route, Router, Routes} from "react-router-dom";
 import './App.css'
 import NavBar from './components/NavBar'
 import Home from './pages/Home'
+import GeneratePhoto from "./pages/GeneratePhoto";
+import RemoveBackground from "./pages/RemoveBackground";
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div>
+    <Router>
+      <div>
       <NavBar/>
-      <Home/>
-
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/generate-photo" element={<GeneratePhoto/>}/>
+          <Route path='/remove-background' element={<RemoveBackground/>}/>
+        </Routes>
+      </main>
       <footer className='bg-gray-800 text-white p-4 mt-auto text-center'>
         <p>© 2025 AI Photo App. All rights reserved.</p>
       </footer>
     </div>
+    </Router>
+    
   )
 }
 
