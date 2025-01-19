2bfbfe56aa838b35e7a6938521de14932c7ac1a2
farruop <rafayKhanpvt@gmail.com>
2025-01-19 23:10:28 +0530

    Add: basic functionality useState

--- src/pages/GeneratePhoto.jsx
+++ src/pages/GeneratePhoto.jsx
@@ -1,6 +1,20 @@
-import React from 'react';
+import React {useState} from 'react';
+
 
 const GeneratePhoto = () => {
+
+  const [useInput, setUserInput]= useState();
+  const [generateImage, setGenerateImage]= useState();
+
+  const handelSubmit=()=>{
+    if(useInput.trim()){
+      setGenerateImage("url");
+    }else{
+      alert("Enter valid promt")
+    }
+  }
+
+
   return (
     <div className="bg-gray-100 min-h-screen flex flex-col items-center p-8">
       <h2 className="text-3xl font-bold text-gray-800 mb-6">Generate AI Photo</h2>
@@ -8,19 +22,21 @@ const GeneratePhoto = () => {
         <input
           type="text"
           placeholder="Enter a prompt"
-          className="w-full p-3 mb-4 border border-gray-300 rounded shadow-sm"
+          className="w-full p-3 mb-4 border border-gray-300 rounded shadow-sm" 
+          onchange={(e)=> setUserInput(e.target.value)}
         />
-        <button className="w-full bg-blue-500 text-white py-2 rounded shadow">
+        <button className="w-full bg-blue-500 text-white py-2 rounded shadow" onchange={handelSubmit}>
           Generate
         </button>
       </div>
       <div className="mt-6">
-        <h3 className="text-lg text-gray-700 mb-2">Generated Image:</h3>
+        <h3 className="text-lg text-gray-700 mb-2">Generated Image: </h3>
         <div className="w-64 h-64 bg-gray-300 border border-gray-300 rounded flex items-center justify-center">
-          <p className="text-gray-500">No image yet</p>
+          <p className="text-gray-500">No image yet </p>
         </div>
       </div>
     </div>
+
   );
 };
 
