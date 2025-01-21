0223e4f455c5f9d22f964b00ea891852abe42183
farruop <rafayKhanpvt@gmail.com>
2025-01-21 22:40:27 +0530

    Add: HandelFile for uploaded image

--- src/pages/RemoveBackground.jsx
+++ src/pages/RemoveBackground.jsx
@@ -1,4 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+const[uploadedImage, setUploadedImage]= useState(null);
+
+const handelFileChange =(event)=>{
+
+  const file= event.target.file[0];
+  if(file){
+    const reader=new FileReader();
+    reader.onload=()=>{
+      setUploadedImage(reader.result)
+    }
+    reader.readAsDataURL(file);
+  }
+}
+
+
 
 const RemoveBackground = () => {
   return (
@@ -9,16 +25,25 @@ const RemoveBackground = () => {
           type="file"
           accept="image/*"
           className="w-full p-3 mb-4 border border-gray-300 rounded shadow-sm"
+          onChange={handelFileChange}
         />
         <button className="w-full bg-green-500 text-white py-2 rounded shadow">
           Remove Background
         </button>
       </div>
+      
       <div className="mt-6">
         <h3 className="text-lg text-gray-700 mb-2">Processed Image:</h3>
+        {uploadedImage ? (
+          <div>
+            <img src={uploadedImage} alt="Uploaded" className='w-64 h-64 object-cover border border-gray-300 rounded' />
+          </div>
+        ) : (
         <div className="w-64 h-64 bg-gray-300 border border-gray-300 rounded flex items-center justify-center">
           <p className="text-gray-500">No image yet</p>
         </div>
+        )}
+        
       </div>
     </div>
   );
